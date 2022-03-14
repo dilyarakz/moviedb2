c1bad207b597a1742607456d7bc8e7b44268ce7e
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-14 01:02:00 -0700

    removed props and styling from header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Header from './components/Header';
 
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,29 +6,18 @@ import { Link } from "react-router-dom";
 
 
 const Header = (props) => {
-
   return (
-    <Flex
-      className="header"
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding={10}
-      bg="#272727"
-      color="#14A76C"
-      {...props}
-    >
+    <Flex>
       <Flex align="center" mr={5}>
         <Link id="header-icon" to="/">
           <Heading as="h1" size="lg" letterSpacing={"tighter"}>
             MovieDB
-          </Heading>
+           </Heading>
         </Link>
       </Flex>
 
       <Box >
-        <Link className="header-fav-box-link" to="#">
+        <Link className="header-fav-box-link" to="/">
           <Text style={{ marginRight: "0.7rem" }}>Favourite </Text>
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fillRule="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
             <path id="heartIcon" fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
