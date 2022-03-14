64c501a5d3084a7b1c8d4a2be27154e1aa99e962
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-13 23:38:37 -0700

    removed components home, moviedetails

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <h1>My app</h1>
     </div>
   );
 }
