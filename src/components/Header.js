96b8f0dcf4da876db5c8bbe9b727a8fb6e7d858e
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-14 01:07:45 -0700

    removed svg icon from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,21 @@ import { Link } from "react-router-dom";
 
 
 
-const Header = (props) => {
+const Header = () => {
   return (
     <Flex>
       <Flex align="center" mr={5}>
-        <Link id="header-icon" to="/">
-          <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-            MovieDB
+
+        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+          MovieDB
            </Heading>
-        </Link>
+
       </Flex>
 
       <Box >
         <Link className="header-fav-box-link" to="/">
           <Text style={{ marginRight: "0.7rem" }}>Favourite </Text>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fillRule="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-            <path id="heartIcon" fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-          </svg>
+
         </Link>
       </Box>
     </Flex>
