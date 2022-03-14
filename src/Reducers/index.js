fec63e64041e64f42bf6baee3cfa9814b2946932
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-13 23:43:56 -0700

    removed reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,19 +1,9 @@
 import { combineReducers } from "redux";
 import getMoviesReducer from "./getMoviesReducer";
-import getSearchedMoviesReducer from "./getSearchedMoviesReducer";
-import getMovieReducer from "./getMovieReducer";
-import getRecomMoviiesReducer from "./getRecomMoviesReducer"
-import favouriteReducer from "./favMoviereducer";
-import getGenresReducer from "./getGenresReducer";
 
 
 
 
 export default combineReducers({
-  movies: getMoviesReducer,
-  search_movie: getSearchedMoviesReducer,
-  movie: getMovieReducer,
-  recommov: getRecomMoviiesReducer,
-  fmovies: favouriteReducer,
-  genres: getGenresReducer
+  movies: getMoviesReducer
 });
