232c81c54bc83e2488a413019e6e2816170c88c7
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-14 02:24:49 -0700

    removed redux logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,31 +7,32 @@ import { getMovies } from '../actions/moviesServer';
 
 const Home = () => {
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMovies());
-  }, [])
+  // const dispatch = useDispatch();
 
-  const movies = useSelector(state => state.movies)
-  movies.movies.map(m => console.log(m))
-  console.log(movies)
+  // useEffect(() => {
+  //   dispatch(getMovies());
+  // }, [])
+
+  // const movies = useSelector(state => state.movies)
+  // movies.movies.map(m => console.log(m))
+  // console.log(movies)
   return (
     <div>
       <Flex style={{ overflowY: "hidden", hight: "70%" }}>
         {
-          movies.movies.map((res) => {
-            if (typeof res !== 'undefined') {
-              return (
-                <WrapItem key={res.id}>
-                  <MovieCard
-                    key={res.id}
-                    movie={res}
-                    width={400}
-                    height={700} />
-                </WrapItem>
-              )
-            }
-          })
+          // movies.movies.map((res) => {
+          //   if (typeof res !== 'undefined') {
+          //     return (
+          //       <WrapItem key={res.id}>
+          //         <MovieCard
+          //           key={res.id}
+          //           movie={res}
+          //           width={400}
+          //           height={700} />
+          //       </WrapItem>
+          //     )
+          //   }
+        })
         }
       </Flex>
     </div>);
