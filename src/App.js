8fe06a677e43746c349828d566ee512230c46800
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-14 00:56:58 -0700

    heroku not working with header added, removing header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
-import Header from './components/Header';
+import Home from "./components/Home";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
